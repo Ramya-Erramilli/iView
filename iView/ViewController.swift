d9beb3521e377eeb037bd7d1192c6359d5de4c62
Ramya Erramilli <rams.sesh.103@gmail.com>
2020-02-08 12:59:37 +0530

    "Initial commit"

--- iView/ViewController.swift
+++ iView/ViewController.swift
@@ -8,13 +8,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var pictures = [String]()
+//    var names = ["Tom","Jerry","JhonnyBravo","PowerPuffGirls","Dexter","Tweety","Oswald","BugsBunny","Popeye","Pingu","RoadRunner","ScoobyDoobyDoo","TeleTubbies"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let fileManager = FileManager.default
+        let path = Bundle.main.resourcePath!
+//        print(path)
+        let items = try! fileManager.contentsOfDirectory(atPath: path+"/Content")
+
+        for item in items{
+                pictures.append(item)
+        }
+        
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "picture", for: indexPath)
+        
+        cell.textLabel?.text = pictures[indexPath.row]
+        print("here")
+        return cell
+        
+        
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let newVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "detailVC") as? DetailViewController{
+            newVC.selectedImage = pictures[indexPath.row]
+            
+            navigationController?.pushViewController(newVC, animated: true)
+        }
+        
+        
+    }
+    
+    
+    
+    // MARK : - FM using URL
+//    static func loadImagesFromAlbum(folderName:String) -> [String]{
+//
+//        let nsDocumentDirectory = FileManager.SearchPathDirectory.documentDirectory
+//        let nsUserDomainMask    = FileManager.SearchPathDomainMask.userDomainMask
+//        let paths               = NSSearchPathForDirectoriesInDomains(nsDocumentDirectory, nsUserDomainMask, true)
+//        var theItems = [String]()
+//        if let dirPath          = paths.first
+//        {
+//            let imageURL = URL(fileURLWithPath: dirPath).appendingPathComponent(folderName)
+//
+//            do {
+//                theItems = try FileManager.default.contentsOfDirectory(atPath: imageURL.path)
+//                return theItems
+//            } catch let error as NSError {
+//                print(error.localizedDescription)
+//                return theItems
+//            }
+//        }
+//        return theItems
+//    }
+
+    
+    
 
 }
 
